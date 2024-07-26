034507e51421831159b59fb01f72494c4acbd1a9
hesam <hesam.hb.bagherpour@gmail.com>
2024-07-26 16:47:06 +0330

    [add] setup folder and StateMachine and Logger

---
+++ Assets/AS.Virtual-Soldier-Ops/Packages/StateMachine/Condition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AS.Virtual_Soldier_Ops.Packages.StateMachine
+{
+    public abstract class Condition
+    {
+        #region Public Methods
+        public abstract bool CheckCondition();
+        #endregion
+    }
+
+    public class DelegatedCondition : Condition
+    {
+        private readonly Func<bool> _func;
+
+        public DelegatedCondition(Func<bool> func)
+        {
+            _func = func;
+        }
+        public override bool CheckCondition()
+        {
+            return _func.Invoke();
+        }
+    }
+}
