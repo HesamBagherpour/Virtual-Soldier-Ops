fa7d8b0d38c4baa417c096fd91cad62a314f9a3a
Hesam <hesam.hb.bagherpour@gmail.com>
2024-07-27 10:55:20 +0330

    [add] creat Tutorial Segment and behavior

---
+++ Assets/AS.Virtual-Soldier-Ops/Packages/Tutorial/AnimatedArrow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AS.Virtual_Soldier_Ops.Packages.Tutorial
+{
+    public class AnimatedArrow : HighlightBehavior
+    {
+        [SerializeField] private List<GameObject> arrowObject;
+        public override void Show()
+        {
+            foreach (var o in arrowObject)
+            {
+                o.SetActive(true);
+            }
+         
+        }
+
+        public override void Hide()
+        {
+            foreach (var o in arrowObject)
+            {
+                o.SetActive(false);
+            }
+        }
+
+
+
+    }
+}
---
+++ Assets/AS.Virtual-Soldier-Ops/Packages/Tutorial/HighlightBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AS.Virtual_Soldier_Ops.Packages.Tutorial
+{
+    public abstract class HighlightBehavior : MonoBehaviour
+    {
+
+        private List<GameObject> _highlightObjects;
+
+
+        public void Init(List<GameObject> objects)
+        {
+            _highlightObjects = objects;
+        }
+
+        public abstract void Show();
+        public abstract void Hide();
+
+
+    }
+}
