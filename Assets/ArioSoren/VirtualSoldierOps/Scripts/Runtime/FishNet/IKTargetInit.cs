442d9d5812380913a24c1d0f4c1bad93ee48ca37
EbiPenMan <karimi.gamedev@gmail.com>
2024-08-07 10:54:46 +0330

    [Add] starterAsset for PlayerArmature and synce

---
+++ Assets/ArioSoren/VirtualSoldierOps/Scripts/Runtime/FishNet/IKTargetInit.cs
@@ -0,0 +1,30 @@
+﻿using RootMotion.FinalIK;
+using UnityEngine;
+namespace ArioSoren.VirtualSoldierOps
+{
+public class IKTargetInit : MonoBehaviour
+{
+	public VRPlayerRefrences playerRefrences;
+    // Start is called before the first frame update
+    public  void Start()
+	{
+		if(playerRefrences == null)
+        	playerRefrences = FindAnyObjectByType<VRPlayerRefrences>();
+        VRIK ikRefrences = GetComponent<VRIK>();
+        if (ikRefrences.solver.spine.headTarget == null)
+        {
+            ikRefrences.solver.spine.headTarget = playerRefrences.headTargetIK;
+        }
+        if (ikRefrences.solver.rightArm.target == null)
+        {
+            ikRefrences.solver.rightArm.target = playerRefrences.handRTargetIK;
+        }
+        if (ikRefrences.solver.leftArm.target == null)
+        {
+            ikRefrences.solver.leftArm.target = playerRefrences.handLTargetIK;
+        }
+
+        transform.position = playerRefrences.transform.position;
+    }
+}
+}
