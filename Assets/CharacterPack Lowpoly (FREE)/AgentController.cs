daaf1e1acdcf3fc7688cc48c3be834e8d05d4eee
masood <masood_a_t@yahoo.com>
2024-08-28 14:02:08 +0400

    add character

---
+++ Assets/CharacterPack Lowpoly (FREE)/AgentController.cs
@@ -0,0 +1,28 @@
+using BehaviorDesigner.Runtime.Tasks.Unity.UnityAnimator;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class AgentController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private int _walkingStateHash = Animator.StringToHash("Walking");
+    private int _idleStateHash = Animator.StringToHash("Idle");
+
+
+    private void Start()
+    {
+        
+    }
+
+    public void SetStateWalking()
+    {
+        _animator.Play(_walkingStateHash);
+    }  
+    public void SetStateIdle()
+    {
+        _animator.Play(_idleStateHash);
+    }
+}
