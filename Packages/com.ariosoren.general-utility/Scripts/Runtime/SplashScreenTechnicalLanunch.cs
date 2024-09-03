2a9bdc0a17583ac140613c45c469d79e007dee6e
Hesam <hesam.hb.bagherpour@gmail.com>
2024-09-03 17:09:51 +0330

    [add] SplashScreen Technical Lanunch

---
+++ Packages/com.ariosoren.general-utility/Scripts/Runtime/SplashScreenTechnicalLanunch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace ArioSoren.GeneralUtility
+{
+    public class SplashScreenTechnicalLanunch : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("start");
+            if (!PlayerPrefs.HasKey("Technical:Launch:FirstTime:SplashScreen"))
+            {
+            
+                Debug.Log("tech events called");
+                PlayerPrefs.SetInt("Technical:Launch:FirstTime:SplashScreen",1);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
